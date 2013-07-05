e1fbff068610c04c0c80ada98de7059b6795001e
ITDeveloper1984 <ITDeveloper1984@gmail.com>
2013-07-05 00:57:45 -0500

    Commit
    Commit

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccessModifiers
+{
+     class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Program p = new Program();
+            //p.Emptymethod();     
+            mod m = new mod();
+            m.Emptymenthod();
+            
+
+        }
+    }
+
+     //class mod1
+     //{
+     //     protected void Emptymethod()
+     //    {
+     //        Console.WriteLine("I am inside Empty method())");
+     //        Console.ReadLine();
+     //    }
+     //}
+
+     
+}
---
+++ AccessModifiers/AccessModifiers/mod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccessModifiers
+{
+     class mod
+    {
+        public void Emptymenthod()
+        {
+            Console.WriteLine("I am inside Empty method())");
+            Console.ReadLine();
+        }
+    }
+}
